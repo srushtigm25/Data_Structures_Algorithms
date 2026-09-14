87171b16b2dac38bff819323a1956584385cd9ff
Srushti Gadera Manjunathappa <38344585+srushtigm25@users.noreply.github.com>
2026-09-14 16:19:00 -0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        left = 0
+        right = len(nums)-1
+        
+
+        while left <= right:
+            mid = (left + right) //2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid +1
+            elif nums[mid] > target:
+                right = mid - 1
+        return -1
+
+
+        # Time complexity: O(log n)
+# Space complexity: O(1)
+
+# Search Insert Position.
+
+     
+        # left = 0
+        # right = len(nums)-1
+        
+
+        # while left <= right:
+        #     mid = (left + right) //2
+
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         left = mid +1
+        #     elif nums[mid] > target:
+        #         right = mid - 1
+        # return left
