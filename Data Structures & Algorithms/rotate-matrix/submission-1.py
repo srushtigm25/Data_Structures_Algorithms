23ad35f5463489adeaacdf2d17c4f64371405b58
Srushti Gadera Manjunathappa <38344585+srushtigm25@users.noreply.github.com>
2026-09-17 19:45:10 -0700

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # 90° clockwise = transpose + reverse each row
+#         The important part is the transpose. We swap:
+
+# matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+
+        n = len(matrix)
+            # Step 1: Transpose
+        for r in range(n):
+            for c in range(r + 1, n):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+        # Step 2: Reverse each row
+        for row in matrix:
+            row.reverse()
+
+#             Time: O(n²)
+
+# Space: O(1), since everything happens in-place.
